cd46bbc19efe16d4376389161a95489bfc86bdaf
Lyamir <xlatiax@gmail.com>
2020-08-26 20:00:03 +0800

    added entry point, npm modules, and directories

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+
