04d9efe78cf4c2a203542f19c429eeaa107f1ecb
Lyamir <xlatiax@gmail.com>
2020-08-26 20:25:09 +0800

    added modules and fixed entry point

--- index.js
+++ index.js
@@ -6,4 +6,38 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
 
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('*', (req, res) => {
+	res.render('error', {
+		title: '404 Error',
+		status: '404',
+		errormsg: 'Page not found'
+	});
+});
+
+
+
+app.listen(PORT, () => {
+	console.log(`Listening to localhost on port ${PORT}`);
+});
