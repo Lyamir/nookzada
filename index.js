6e77dbc8c1665d7a291c41a893c905f8ece6eb72
Lyamir <xlatiax@gmail.com>
2020-08-28 00:35:03 +0800

    added database connection

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+/* 
+mongo "mongodb+srv://cluster0.fx7fb.gcp.mongodb.net/nookzada" --username angels
+
+password: ccapdev
+*/
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
@@ -15,10 +21,40 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.engine('hbs', exphbs({
+
+//DB CONNECTION
+mongoose.connect('mongodb+srv://angels:ccapdev@cluster0.fx7fb.gcp.mongodb.net/nookzada?retryWrites=true&w=majority', {useNewUrlParser: true}, (err)=>{
+	if(err)
+		console.error(err)
+	else
+		console.log("Connection successful")
+});
+
+//ITEMS COLLECTION
+let itemsModel = mongoose.model('items', ({}, {strict: false})) 
+itemsModel.find({},(err, item)=>{
+	if(err)
+		console.log(err)
+	else
+		console.log(item)
+})
+
+//USERS COLLECTION
+let usersModel = mongoose.model('users', ({}, {strict: false})) 
+usersModel.find({},(err, item)=>{
+	if(err)
+		console.log(err)
+	else
+		usersModel.count({}, (err, count)=>{
+			console.log(count);
+		})
+})
+
+
+/*app.engine('hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs'
-}));
+}));*/
 
 app.set('view engine', 'hbs');
 
